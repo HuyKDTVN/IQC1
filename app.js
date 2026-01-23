4d609ad67b80adae595d53844069414d8b1c8f23
HuyKDTVN <huy.ld.352@dtvn.kyocera.com>
2026-01-23 15:23:04 +0700

    Add files via upload

--- app.js
+++ app.js
@@ -3,56 +3,56 @@ const inputDS = document.getElementById("barcodeDS");
 const readerDiv = document.getElementById("reader");
 let html5QrCode;
 
-// inputDS.addEventListener("click", () => {
-//     readerDiv.style.display = "block";
+inputDS.addEventListener("click", () => {
+    readerDiv.style.display = "block";
 
-//     html5QrCode = new Html5Qrcode("reader");
+    html5QrCode = new Html5Qrcode("reader");
 
-//     html5QrCode.start(
-//         { facingMode: "environment" }, // camera sau
-//         {
-//             fps: 10,
-//             qrbox: 250
-//         },
-//         (decodedText) => {
-//             inputDS.value = decodedText;
-//             stopScanner();
+    html5QrCode.start(
+        { facingMode: "environment" }, // camera sau
+        {
+            fps: 10,
+            qrbox: 250
+        },
+        (decodedText) => {
+            inputDS.value = decodedText;
+            stopScanner();
             
-//         },
-//         (errorMessage) => {
-//             // ignore scan errors
-//         }
-//     );
-// });
+        },
+        (errorMessage) => {
+            // ignore scan errors
+        }
+    );
+});
 
  const inputChiThi = document.getElementById("barcodeChiThi");
 const readerDiv2 = document.getElementById("reader2");
 
-// inputChiThi.addEventListener("click", () => {
-//     readerDiv2.style.display = "block";
-//     html5QrCode = new Html5Qrcode("reader");
-//     html5QrCode.start(
-//         { facingMode: "environment" }, // camera sau
-//         {
-//             fps: 10,
-//             qrbox: 250
-//         },
-//         (decodedText) => {
-//             inputChiThi.value = decodedText;
-//             stopScanner();
-//         },
-//         (errorMessage) => {
-//             // ignore scan errors
-//         }
-//     );
-// });
-// function stopScanner() {
-//     if (html5QrCode) {
-//         html5QrCode.stop().then(() => {
-//             readerDiv2.style.display = "none";
-//         });
-//     }
-// }
+inputChiThi.addEventListener("click", () => {
+    readerDiv2.style.display = "block";
+    html5QrCode = new Html5Qrcode("reader");
+    html5QrCode.start(
+        { facingMode: "environment" }, // camera sau
+        {
+            fps: 10,
+            qrbox: 250
+        },
+        (decodedText) => {
+            inputChiThi.value = decodedText;
+            stopScanner();
+        },
+        (errorMessage) => {
+            // ignore scan errors
+        }
+    );
+});
+function stopScanner() {
+    if (html5QrCode) {
+        html5QrCode.stop().then(() => {
+            readerDiv2.style.display = "none";
+        });
+    }
+}
 const btnScan = document.getElementById("buttonConfirm");
 const result = document.getElementById("ket-qua");
 btnScan.addEventListener("click", () => {
