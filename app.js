073656e000ba0e5676673df6777f3183b71b20ff
HuyKDTVN <huy.ld.352@dtvn.kyocera.com>
2026-08-11 15:31:40 +0700

    Add files via upload

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ let html5QrCode;
 
 inputDS.addEventListener("click", () => {
 
-    alert ("test");
+    alert ("test 2");
     result.textContent = "";
     result.style.backgroundColor = "white";
 
@@ -35,7 +35,7 @@ inputDS.addEventListener("click", () => {
     ).then(async () => {
         try {
             await html5QrCode.applyVideoConstraints({
-                advanced: [{ zoom: 5.0 }]
+                advanced: [{ zoom: 2.0 }]
             });
         } catch (err) {
             alert("Thiết bị không hỗ trợ zoom 2x:", err);
@@ -71,7 +71,7 @@ inputChiThi.addEventListener("focus", () => {
     ).then(async () => {
         try {
             await html5QrCode.applyVideoConstraints({
-                advanced: [{ zoom: 5.0 }]
+                advanced: [{ zoom: 2.0 }]
             });
         } catch (err) {
             alert("Thiết bị không hỗ trợ zoom 2x:", err);
@@ -101,7 +101,7 @@ inputCaseMark.addEventListener("focus", () => {
     ).then(async () => {
         try {
             await html5QrCode.applyVideoConstraints({
-                advanced: [{ zoom: 5.0 }]
+                advanced: [{ zoom: 2.0 }]
             });
         } catch (err) {
             alert("Thiết bị không hỗ trợ zoom 2x:", err);
